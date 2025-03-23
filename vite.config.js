81eb2c4fe42ce0bbb335fc6d8737497a7b14e7ba
Adrien Richard <adrien.richard.contact@gmail.com>
2025-03-23 16:42:36 +0100

    fix: mainjs pages path

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { fileURLToPath } from 'node:url';
 
 export default {
-    base: "/portfolio/",
-    rollupOptions: {
-        external: [
-            fileURLToPath(
-                new URL(
-                    '/portfolio/src/main.js',
-                    import.meta.url
-                )
-            )
-        ]
-    }
+    base: "/portfolio/"
 }
